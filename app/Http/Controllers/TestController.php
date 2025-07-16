6047c25f00e7864fc277119c0adbef190a6a0735
huucanh0707 <24130029@st.hcmuaf.edu.vn>
2025-07-16 15:20:16 +0700

    fix return funtion ...

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,12 +7,6 @@ use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller {
     public function hello() {
-        $connectionName = DB::getDefaultConnection();
-        $config = config("database.connections.$connectionName");
-
-        return response()->json([
-            'current_connection' => $connectionName,
-            'config' => $config,
-        ]);
+        return view('hello');
     }
 }
